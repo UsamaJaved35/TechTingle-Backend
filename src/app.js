40aac5f5ef7050a97fe46b5434855f72b67cb739
UsamaJaved007 <usama.javed@dubizzlelabs.com>
2024-11-09 23:14:18 +0500

    added test route

--- src/app.js
+++ src/app.js
@@ -19,6 +19,12 @@ app.use(cors(
 app.use(express.json())
 app.use(cookieParser())
 
+app.use("/", (req, res, next) => {
+    res.send("Hello from the server")
+})
+app.use("/xyz", (req, res, next) => {
+    res.send("Hello from xyz")
+})
 app.use("/auth", authRouter)
 app.use("/", profileRouter)
 app.use("/",requestRouter)
